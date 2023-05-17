4af7cc08568341a94f789a4ce008ab93ebb53b43
+ <+>
2023-05-16 22:06:35 -0400

    add ls and mkdir

--- lib/cli.py
+++ lib/cli.py
@@ -1,16 +1,24 @@
 import os
-
+from os import listdir
 terminal_active = True
 
 while terminal_active == True:
     directory = os.getcwd()
-    x = input(f'[{os.getlogin()}@{os.uname().sysname}-{os.uname().machine}] {directory}$ ')
+    x = input(
+        f'[{os.getlogin()}@{os.uname().sysname}-{os.uname().machine}] {directory}$ ')
     if x == "exit":
         terminal_active = False
     elif x.split(" ")[0] == "cd":
         if len(x.split(" ")) > 2:
             print("Too many arguments given!")
         else:
-            os.chdir(x.split(" ")[1])
-    elif x == "ls":
-        pass
+            if os.chdir(x.split(" ")[1]) == "/":
+                os.chdir(x.split(" ")[1])
+    elif x.split(" ")[0] == "ls":
+        print(*(os.listdir(directory)),sep="\n")
+    elif x.split(" ")[0] == "mkdir":
+        if len(x.split(" ")) > 2:
+            print("Too many arguments given!")
+        else:
+            if os.mkdir(x.split(" ")[1]) == "/":
+                os.mkdir(x.split(" "))
