0a51a3759fc5aab78b09b2d7c1c80e97ecd664d7
+ <+>
2023-05-16 22:08:17 -0400

    revert cd

--- lib/cli.py
+++ lib/cli.py
@@ -12,8 +12,7 @@ while terminal_active == True:
         if len(x.split(" ")) > 2:
             print("Too many arguments given!")
         else:
-            if os.chdir(x.split(" ")[1]) == "/":
-                os.chdir(x.split(" ")[1])
+            os.chdir(x.split(" ")[1])
     elif x.split(" ")[0] == "ls":
         print(*(os.listdir(directory)),sep="\n")
     elif x.split(" ")[0] == "mkdir":
